8185d6c4aa6a9767144a9500065d2251dc31abbe
Anamul Haque <anamulhaque9901@gmail.com>
2024-07-03 21:56:29 +0600

    re-usable admin routes and sidbar Lyout

---
+++ src/pages/admin/CreateAdmin.tsx
@@ -0,0 +1,10 @@
+
+const CreateAdmin = () => {
+    return (
+        <div>
+            <h1>this is create faculty component</h1>
+        </div>
+    );
+};
+
+export default CreateAdmin;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,53 +1,37 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
-import AdminLayout from "../components/layout/AdminLayout";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
   {
     path: "/admin",
-    // element: <AdminLayout />,
-    element: <App/>,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-    ],
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminRoutes,
   },
 
   {
-    path: "/",
-    element: <Contact />,
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: "/contact",
-    element: <Contact />,
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
