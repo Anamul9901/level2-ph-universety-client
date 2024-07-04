a9316c5a6c279b9eed3ec5bb5d89fd4a69f7bc20
Anamul Haque <anamulhaque9901@gmail.com>
2024-07-04 11:11:52 +0600

    sidebar items reusesable

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,7 @@
-import { Layout, Menu, MenuProps } from "antd";
-import { NavLink, Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout } from "antd";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+const { Header, Content } = Layout;
 const MainLayout = () => {
   // const items: MenuProps["items"] = [
   //   {
@@ -28,38 +28,9 @@ const MainLayout = () => {
   //   },
   // ];
 
-
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          PH Uni
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          // items={items}
-          items={adminSidebarItems}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
@@ -72,9 +43,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export * from "./sidebar.type";
+
+//* eitake berel kore dua bole. ekhon tyes folder er sob file ei index file theke import kora jebe
