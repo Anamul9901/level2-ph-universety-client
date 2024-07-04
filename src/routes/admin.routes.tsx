69c0b33ca07a9816668653bea3d1de159ae805b3
Anamul Haque <anamulhaque9901@gmail.com>
2024-07-04 09:43:39 +0600

    make reusable routes

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -13,10 +13,10 @@ type TRoute = {
 type TSidebarItem = {
   key: string;
   label: ReactNode;
-  children?: TSidebarItems[];
+  children?: TSidebarItem[];
 };
 
-const adminPaths = [
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -45,25 +45,25 @@ const adminPaths = [
 ];
 
 //* Programatical way
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.name) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.name) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
 
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       acc.push({
+//         path: child.path,
+//         element: child.element,
+//       });
+//     });
+//   }
 
-  return acc;
-}, []);
+//   return acc;
+// }, []);
 
 export const adminSidebarItems = adminPaths.reduce(
   (acc: TSidebarItem[], item) => {
