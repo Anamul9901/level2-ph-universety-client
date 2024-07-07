d03ac935107d17c8b6434a078dda36ecae09b805
Anamul Haque <anamulhaque9901@gmail.com>
2024-07-07 17:59:42 +0600

    if refress token unvalid then ueser logut auto

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -16,25 +16,31 @@ const baseQuery = fetchBaseQuery({
 
 // refresh kore new access token generate kore debe
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions);
 
   if (result.error?.status === 401) {
-    console.log("Sending refresh toen");
 
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
       method: "POST",
       credentials: "include",
     });
     const data = await res.json();
-    console.log(data);
-    const user = (api.getState() as RootState).auth.user;
 
-    api.dispatch(
-      setUser({
-        user,
-        token: data?.data.accessToken,
-      })
-    );
+    // backend theke 2 ta token ase. ekta jwt token arekta refress token. jode refress token er data over hoye jai tahole accessToken null hoye jabe. fole user auto logout kore debo.
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data?.data.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
   }
   return result;
 };
